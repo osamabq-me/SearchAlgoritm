5c9bd45a40e7467eeee65b9358b7e8d740ed202c
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-10-28 16:00:49 +0700

    Add project files.

---
+++ SearchAlgoritm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgoritm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+
+        public void linersearch()
+        {
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
